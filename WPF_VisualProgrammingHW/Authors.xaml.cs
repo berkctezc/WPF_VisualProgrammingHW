09e711d25aa8b139c839034575f30eaeb76e61e2
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2021-06-24 22:36:19 +0300

    author update implemented

--- WPF_VisualProgrammingHW/Authors.xaml.cs
+++ WPF_VisualProgrammingHW/Authors.xaml.cs
@@ -44,20 +44,9 @@ namespace WPF_VisualProgrammingHW
 
         private void DataGrid_OnSelectedCellsChanged(object sender, SelectedCellsChangedEventArgs e)
         {
-            TBL_AUTHOR author = new TBL_AUTHOR
-            {
-                ID = 1,
-                NAME = "a",
-                SURNAME = "b",
-                STATUS = true
-            };
-            
-
-            Console.WriteLine(author.ID);
-
-            txtId.Text = author.ID.ToString();
-            txtName.Text = author.NAME;
-            txtSurname.Text = author.SURNAME;
+            txtId.Text = (dg.SelectedItem as TBL_AUTHOR)?.ID.ToString();
+            txtName.Text = (dg.SelectedItem as TBL_AUTHOR)?.NAME;
+            txtSurname.Text = (dg.SelectedItem as TBL_AUTHOR)?.SURNAME;
         }
 
         private void btnCreate_Click(object sender, RoutedEventArgs e)
@@ -70,6 +59,23 @@ namespace WPF_VisualProgrammingHW
             };
 
             _db.TBL_AUTHOR.Add(newAuthor);
+            _db.SaveChanges();
+            Read();
+        }
+
+        private void btnRead_Click(object sender, RoutedEventArgs e)
+        {
+            Read();
+        }
+
+        private void btnUptd_Click(object sender, RoutedEventArgs e)
+        {
+            int authorId = (dg.SelectedItem as TBL_AUTHOR).ID;
+
+            TBL_AUTHOR authorToUpdate = (from a in _db.TBL_AUTHOR where a.ID == authorId select a).Single();
+            authorToUpdate.NAME = txtName.Text;
+            authorToUpdate.SURNAME = txtSurname.Text;
+
             _db.SaveChanges();
             Read();
         }
