cb21f0d1e6621dd69ccca048b3b0f2931f35a43c
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2021-06-24 21:40:47 +0300

    create operation and refreshing of content, databindings

--- WPF_VisualProgrammingHW/Authors.xaml.cs
+++ WPF_VisualProgrammingHW/Authors.xaml.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using System.Data;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using Newtonsoft.Json;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -26,7 +28,7 @@ namespace WPF_VisualProgrammingHW
             InitializeComponent();
         }
 
-        private DbLibrarySystemEntities db = new DbLibrarySystemEntities();
+        private DbLibrarySystemEntities _db = new DbLibrarySystemEntities();
 
         private void Authors_Load(object sender, EventArgs e)
         {
@@ -36,14 +38,40 @@ namespace WPF_VisualProgrammingHW
 
         void Read()
         {
-            DataGrid.ItemsSource = (from x in db.TBL_AUTHOR
-                select new
-                {
-                    x.ID,
-                    x.NAME,
-                    x.SURNAME,
-                    x.STATUS
-                }).Where(x => x.STATUS == true).ToList();
+            dg.ItemsSource = null;
+            dg.ItemsSource = _db.TBL_AUTHOR.ToList();
+        }
+
+        private void DataGrid_OnSelectedCellsChanged(object sender, SelectedCellsChangedEventArgs e)
+        {
+            TBL_AUTHOR author = new TBL_AUTHOR
+            {
+                ID = 1,
+                NAME = "a",
+                SURNAME = "b",
+                STATUS = true
+            };
+            
+
+            Console.WriteLine(author.ID);
+
+            txtId.Text = author.ID.ToString();
+            txtName.Text = author.NAME;
+            txtSurname.Text = author.SURNAME;
+        }
+
+        private void btnCreate_Click(object sender, RoutedEventArgs e)
+        {
+            TBL_AUTHOR newAuthor = new TBL_AUTHOR()
+            {
+                NAME = txtName.Text,
+                SURNAME = txtSurname.Text,
+                STATUS = true
+            };
+
+            _db.TBL_AUTHOR.Add(newAuthor);
+            _db.SaveChanges();
+            Read();
         }
     }
 }
