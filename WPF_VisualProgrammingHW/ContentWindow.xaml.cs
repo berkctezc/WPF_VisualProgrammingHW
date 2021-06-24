1d70ac769cb0874b1d5e160c1130396cb2db93a6
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2021-06-24 17:12:33 +0300

    content and frame relation initialization and empty author created

---
+++ WPF_VisualProgrammingHW/ContentWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF_VisualProgrammingHW
+{
+    /// <summary>
+    /// Interaction logic for ContentWindow.xaml
+    /// </summary>
+    public partial class ContentWindow : Window
+    {
+        public ContentWindow()
+        {
+            InitializeComponent();
+        }
+        
+        private Authors authorsWindow;
+        private void Authors_Click(object sender, RoutedEventArgs e)
+        {
+            authorsWindow = new Authors();
+            MyFrame.Content = null;
+            MyFrame.Content = authorsWindow;
+        }
+    }
+}
