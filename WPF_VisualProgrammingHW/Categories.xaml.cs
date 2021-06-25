773ade8febf7fced2518e64948633a1a05432d22
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2021-06-25 13:02:14 +0300

    empty categories and publishers created

---
+++ WPF_VisualProgrammingHW/Categories.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_VisualProgrammingHW
+{
+    /// <summary>
+    /// Interaction logic for Categories.xaml
+    /// </summary>
+    public partial class Categories : Page
+    {
+        public Categories()
+        {
+            InitializeComponent();
+        }
+    }
+}
