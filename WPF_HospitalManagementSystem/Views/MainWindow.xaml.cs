c551bc3f7c55197051ac5bba937172ee3c31019a
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2021-06-27 15:06:58 +0300

    login window logo and logic changed

--- WPF_HospitalManagementSystem/Views/MainWindow.xaml.cs
+++ WPF_HospitalManagementSystem/Views/MainWindow.xaml.cs
@@ -11,18 +11,12 @@ namespace WPF_HospitalManagementSystem.Views
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+        public MainWindow()=>InitializeComponent();
 
         private DbHospitalManagementSystemContext _db = new DbHospitalManagementSystemContext();
         private ContentWindow contentWindow;
 
-        private void MainWindow_OnClosed(object? sender, EventArgs e)
-        {
-            Environment.Exit(0);
-        }
+        private void MainWindow_OnClosed(object sender, EventArgs e) => Environment.Exit(0);
 
         private void txtUsername_GotKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
         {
@@ -32,15 +26,16 @@ namespace WPF_HospitalManagementSystem.Views
 
         private void txtPass_GotKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
         {
-            if (txtPass.Password == "password")
+            bool touched = true;
+            if (touched)
                 txtPass.Password = "";
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             var query = (from x in _db.TblAdmins
-                where x.Username == txtUsername.Text & x.Password == txtPass.Password
-                select x);
+                         where x.Username == txtUsername.Text & x.Password == txtPass.Password
+                         select x);
 
             if (query.Any())
             {
