be2de75392467e95307e67f0007390a6dd9b9a17
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2021-06-27 17:36:11 +0300

    viewmodels created

---
+++ WPF_HospitalManagementSystem/ViewModels/NurseViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_HospitalManagementSystem.ViewModels
+{
+    public class NurseViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Policlinic { get; set; }
+        public DateTime? Birthofdate { get; set; }
+        public bool? Status { get; set; }
+    }
+}
